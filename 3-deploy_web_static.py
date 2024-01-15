097810b93676fa428a9c6ac17cac28a3c83a00b2
Wisdom Sena <wisdomsena36@gmail.com>
2024-01-14 21:56:58 -0800

    Add 3-deploy_web_static.py

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Cleans deployment"""
+
+from fabric.api import *
+import os
+from datetime import datetime
+import tarfile
+
+env.hosts = ["100.25.47.204", "100.25.46.46"]
+env.user = "ubuntu"
+
+
+def do_clean(number=0):
+    """ Removes all but given number of archives"""
+    number = int(number)
+    if number < 2:
+        number = 1
+    number += 1
+    number = str(number)
+    with lcd("versions"):
+        local("ls -1t | grep web_static_.*\.tgz | tail -n +" +
+              number + " | xargs -I {} rm -- {}")
+    with cd("/data/web_static/releases"):
+        run("ls -1t | grep web_static_ | tail -n +" +
+            number + " | xargs -I {} rm -rf -- {}")
